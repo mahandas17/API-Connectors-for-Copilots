1517ceda825de6579b243bec49c6d85aac87b613
Mahan Das <mahandas@microsoft.com>
2023-09-14 11:50:19 -0700

    adding nl2api plugin

---
+++ NL2APIPlugin/sk-csharp-chatgpt-plugin/azure-function/APIIdentifier.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace SecurityPlugin
+{
+    public class IdentifyAPI
+    {
+        private readonly ILogger _logger;
+
+        public IdentifyAPI(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<IdentifyAPI>();
+        }
+
+        [OpenApiOperation(operationId: "GetAPI", tags: new[] { "ExecuteFunction" }, Description = "Identify API based on the intent.")]
+        [OpenApiParameter(name: "intent", Description = "The user intent", Required = true, In = ParameterLocation.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "Returns back the API url.")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "text/plain", bodyType: typeof(string), Description = "Displays an error message.")]
+        [Function("IdentifyAPI")]
+        public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            string jintent = req.Query["intent"];
+            string URL = "https://graph.microsoft.com/v1.0/";
+            var jobject = JsonConvert.DeserializeObject<dynamic>(jintent);
+
+            switch (jobject.ToString())
+            {
+                case "SecuirtyIsight":
+                    URL = URL + "security/";
+                    break;
+
+                case "EdgeInsight":
+                    URL = URL + "admin/edge/internetExplorerMode/";
+                    break;
+
+                case "ComplianceInsight":
+                    URL = URL + "beta/compliance/ediscovery/";
+                    break;
+
+            }
+
+            HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+            response.Headers.Add("Content-Type", "text/plain");
+
+            response.WriteString(URL);
+
+            _logger.LogInformation($"Item added to the list.");
+
+            return response;
+        }
+
+
+    }
+}
---
+++ NL2APIPlugin/sk-csharp-chatgpt-plugin/azure-function/AiPluginJson.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Models;
+
+public class AIPluginJson
+{
+    [Function("GetAIPluginJson")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = ".well-known/ai-plugin.json")] HttpRequestData req)
+    {
+        var currentDomain = $"{req.Url.Scheme}://{req.Url.Host}:{req.Url.Port}";
+
+        HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "application/json");
+
+        var appSettings = AppSettings.LoadSettings();
+
+        // serialize app settings to json using System.Text.Json
+        var json = System.Text.Json.JsonSerializer.Serialize(appSettings.AIPlugin);
+
+        // replace {url} with the current domain
+        json = json.Replace("{url}", currentDomain, StringComparison.OrdinalIgnoreCase);
+
+        response.WriteString(json);
+
+        return response;
+    }
+}
+/*
+ * [INSTRUCTIONS]
+Generate a Microsoft Graph API based Odata query to get the user detail based on the user input below
+{{$input}}
+
+
+[query]
+-*------------
+{
+  "schema": 1,
+  "description": "Provides User related Microsoft Graph API endpoint with odata queries based on user input.",
+  "type": "completion",
+  "completion": {
+    "max_tokens": 1000,
+    "temperature": 0.0,
+    "top_p": 0.0,
+    "presence_penalty": 0.0,
+    "frequency_penalty": 0.0
+  },
+  "input": {
+    "parameters": [
+      {
+        "name": "input",
+        "description": "A string that contains a user query",
+        "defaultValue": ""
+      }
+    ]
+  }
+}
+*/
